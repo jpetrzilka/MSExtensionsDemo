e8c8d325a3bb2d7ac2717a0e9c5b78563e508e10
yog <jiri.petrzilka@gmail.com>
2017-11-19 11:56:23 +0100

    refactoring into classes - first attempt

---
+++ CmdUtilsTest/AppBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdUtilsTest
+{
+    class AppBuilder
+    {
+        public CommandLineApplication BuildApp()
+        {
+            var app = new CommandLineApplication();
+            var cmdBuilder = new CommandsBuilder();
+            app.Command("start", cmdBuilder.BuildStartCmd());
+            return app;
+        }
+    }
+}

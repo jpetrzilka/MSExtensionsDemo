b97ae16f8ed7f2c7eabcf3262f9d11438823183b
yog <jiri.petrzilka@gmail.com>
2017-11-19 11:33:42 +0100

    initial commit

---
+++ CmdUtilsTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CmdUtilsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();            
+            app.Command("start", Start);
+            app.Execute(args);
+        }
+
+        static void Start(CommandLineApplication cmd)
+        {
+            var opt = cmd.Option("-s|--services <services>", String.Empty, CommandOptionType.MultipleValue);
+            cmd.OnExecute(() =>
+            {
+                Console.WriteLine("Ahoj " + String.Join(", ",opt.Values));
+                return 0;
+            });
+        }
+    }
+}

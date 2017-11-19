319a3031761553331e3859db9daf5b3dead7b8b3
yog <jiri.petrzilka@gmail.com>
2017-11-19 21:56:31 +0100

    Added stop command

---
+++ CmdUtilsTest/Commands/StopCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace CmdUtilsTest.Commands
+{
+    class StopCommand : IConsoleCmd
+    {
+        public String Name => "Stop";
+
+        public String Description => "Stops the system";
+
+        public Action<CommandLineApplication> Action =>
+            new Action<CommandLineApplication>(cmd =>
+            {
+                cmd.Description = Description;
+                cmd.HelpOption("-?|--help");
+
+                var serviceOpt = cmd.Option("-s|--services <services>",
+                                            "services to Stop separated by comma",
+                                            CommandOptionType.MultipleValue);
+                var dryRunOpt = cmd.Option("-d|--dryrun",
+                                           "displays only steps that will be taken",
+                                           CommandOptionType.NoValue);
+                var forceOpt = cmd.Option("-f|--force",
+                                          "Forces the system shutdown",
+                                          CommandOptionType.NoValue);
+
+                cmd.OnExecute(() =>
+                {
+                    Console.WriteLine("Stopping services {0}",
+                                      String.Join(", ", serviceOpt.Values));
+
+                    if (!dryRunOpt.HasValue())
+                    {
+                        Console.WriteLine("Serivces stopped{0}",
+                                          forceOpt.HasValue() ? " by force" : String.Empty);
+                    }
+
+                    return 0;
+                });
+            });
+    }
+}
